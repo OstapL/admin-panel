04b680bad59df319dc0ba9be6238c249eab001a8
Ostap <ostap.lazoryak@gmail.com>
2023-10-30 17:35:44 +0200

    add firebase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,4 +35,10 @@ export default defineNuxtConfig({
     // Admin dashboard renders only on client-side
     '/admin/**': { ssr: false },
   },
+  publicRuntimeConfig: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY
+  },
+  privateRuntimeConfig: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY
+  },
 })
---
+++ plugins/firebaseAuth.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+
+export default defineNuxtPlugin(nuxtApp => {
+    const config = useRuntimeConfig();
+    const firebaseConfig = {
+        apiKey: config.FIREBASE_API_KEY,
+        authDomain: "admin-panel-3f4e5.firebaseapp.com",
+        projectId: "admin-panel-3f4e5",
+        storageBucket: "admin-panel-3f4e5.appspot.com",
+        messagingSenderId: "42670242007",
+        appId: "1:42670242007:web:fb99b54c4d94a80a499892"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    console.log(app);
+})
